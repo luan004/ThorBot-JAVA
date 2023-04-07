2dfbc7a8796aa670456317a31a8500120885e758
luan004 <luan.gabriel.ifpr@gmail.com>
2023-04-07 19:35:37 -0300

    07-04

--- src/main/java/com/github/luan004/JavaBot/Token.java
+++ src/main/java/com/github/luan004/JavaBot/Token.java
@@ -1,10 +1,6 @@
 package com.github.luan004.JavaBot;
 
 public class Token {
-<<<<<<< HEAD
-=======
-	
->>>>>>> 0db67625398512d2cc17726de6c056866ef5cce7
 	String getToken() {
 		return "TOKEN_AQUI";
 	}
